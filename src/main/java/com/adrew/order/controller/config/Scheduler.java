a828fb723c40b11c6b94d2e10906fed072ea7848
AndrewTkachuk66 <andtka66@gmail.com>
2020-08-01 17:28:19 +0300

    Scheduler fixing

--- src/main/java/com/adrew/order/controller/config/Scheduler.java
+++ src/main/java/com/adrew/order/controller/config/Scheduler.java
@@ -4,15 +4,20 @@ package com.adrew.order.controller.config;
  * Created by Andre on 31.07.2020.
  */
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Slf4j
 @Component
 public class Scheduler {
 
     @Scheduled(fixedDelay = 60000)
-    @Query(value = "DELETE FROM order_controller.orders WHERE order_date_time < (UNIX_TIMESTAMP() - 600);", nativeQuery = true)
+    @Query(value = "delete from order_controller.orders where order_date_time < (now() - '10 minutes'::interval) ", nativeQuery = true)
     public void scheduleFixedDelayTask() {
+        log.info("Scheduler has worked, time - {}", new Date());
     }
 }
