ad8e326d1d1dbb6baf3fbb104b27643249b10ef1
AndrewTkachuk66 <andtka66@gmail.com>
2020-08-01 18:44:11 +0300

    Scheduler works correct

--- src/main/java/com/adrew/order/controller/config/Scheduler.java
+++ src/main/java/com/adrew/order/controller/config/Scheduler.java
@@ -4,7 +4,10 @@ package com.adrew.order.controller.config;
  * Created by Andre on 31.07.2020.
  */
 
+import com.adrew.order.controller.repository.OrderRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.criterion.Order;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,11 +16,14 @@ import java.util.Date;
 
 @Slf4j
 @Component
+@AllArgsConstructor
 public class Scheduler {
 
+    private OrderRepository orderRepository;
+
     @Scheduled(fixedDelay = 60000)
-    @Query(value = "delete from order_controller.orders where order_date_time < (now() - '10 minutes'::interval) ", nativeQuery = true)
     public void scheduleFixedDelayTask() {
-        log.info("Scheduler has worked, time - {}", new Date());
+        orderRepository.deleteAllOrdersOlderTenMinutes();
+        log.info("Scheduler has worked, old orders were deleted time - {}", new Date());
     }
 }
--- src/main/java/com/adrew/order/controller/repository/OrderRepository.java
+++ src/main/java/com/adrew/order/controller/repository/OrderRepository.java
@@ -3,17 +3,27 @@ package com.adrew.order.controller.repository;
 import com.adrew.order.controller.domain.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
  * Created by Andre on 30.07.2020.
  */
+@Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long>, JpaSpecificationExecutor<OrderEntity> {
     @Query(value = "SELECT *\n" +
             "FROM order_controller.orders\n" +
             "WHERE price = (SELECT MIN(price)\n" +
             "             FROM order_controller.orders );",  nativeQuery = true)
     List<OrderEntity> getOrderWithLowestPrice();
+
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from order_controller.orders where order_date_time < (now() - interval '10 minutes');", nativeQuery = true)
+    void deleteAllOrdersOlderTenMinutes();
 }
